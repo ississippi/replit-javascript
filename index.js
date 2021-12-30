f9cc4994a8e2147eb9e10622dc01c10e874ea74f
techartists <>
2021-12-30 00:50:32 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,79 @@
+
+class Node {
+  constructor(value) {
+    this.head = value,
+    this.next = null
+  }
+}
+class LinkedList {
+  constructor(value) {
+    this.head = {
+      value: value,
+      next: null
+    }
+    this.tail = this.head;
+    this.length = 1;
+  }
+
+  append(value) {
+    const newNode = new Node(value);
+    this.tail.next =  newNode;
+    this.tail = newNode;
+    this.length++;
+    return this;
+  }
+
+  prepend(value) {
+    const newNode = new Node(value);
+    newNode.next = this.head;
+    this.head = newNode;
+    this.length++;
+    return this;
+  }
+
+  printList() {
+    const array = [];
+    let currentNode = this.head;
+    while (currentNode != null)
+    {
+      array.push(currentNode.value);
+      currentNode = currentNode.next;
+    }
+    return array;
+  }
+
+  insert(index, value) {
+    if(index >= this.length) {
+      console.log('yes')
+      return this.append(value);
+    }
+    const newNode = new Node(value);
+    const leader = traverseToIndex(index-1);
+    const holdingPointer = leader.next;
+    leader.next = newNode;
+    newNode.next = holdingPointer;
+    this.length++;
+    return this.printList();
+  }
+
+  traverseToIndex(index) {
+   let currentNode = this.head;
+    let idx = 0;
+    while (idx !== index)
+    {
+      currentNode = currentNode.next;
+      idx++;
+    }
+    return currentNode;
+  }
+}
+
+const myLinkedList = new LinkedList(10);
+//console.log(myLinkedList);
+myLinkedList.append(5);
+myLinkedList.append(16);
+myLinkedList.prepend(1);
+myLinkedList.insert(2,99);
+pl = myLinkedList.printList();
+console.log(pl);
+console.log(myLinkedList);
