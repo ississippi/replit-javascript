d82f653956396c091ad4851b62f5592038ec9f40
techartists <37956350+ississippi@users.noreply.github.com>
2021-12-31 09:31:33 +0000

    Queues

---
+++ StackArray.js
@@ -0,0 +1,34 @@
+class Node {
+  constructor(value) {
+    this.value = value,
+    this.next = null
+  }
+}
+
+class Stack {
+  constructor(){
+    this.array = []; 
+  }
+
+  peek() {
+    return this.array[this.array.length - 1];
+  }
+
+  push(value) {
+    this.array.push(value);
+    return this;
+  }
+
+  pop() {
+    this.array.pop();
+    return this;
+  }
+}
+
+const myStack = new Stack();
+myStack.push('Google');
+myStack.push('Udemy');
+myStack.push('Discord');
+myStack.peek();
+//myStack.pop();
+//myStack.pop();
